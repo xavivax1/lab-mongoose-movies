5b09d04f1f47496f0fd7cb8e4e4e8fc44c667cf7
xavivax1 <xavier.deviala@gmail.com>
2019-03-03 18:44:45 +0100

    iteration 3done

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const express = require('express');
 const router = express.Router();
@@ -6,6 +6,10 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/celebrities', (req, res, next) => {
+  res.render('../view/celebrities/index', { title: 'Celebrities' });
 });
 
 module.exports = router;
