b49f7ec6ce0956da8d8bf159045b55699b044700
xavivax1 <xavier.deviala@gmail.com>
2019-03-03 19:55:30 +0100

    Iteration #5 implemented

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -3,10 +3,37 @@ const express = require('express');
 const router = express.Router();
 // const Movie = require('../models/celebrity.');
 
-router.get('/movies', async (req, res, next) => {
+router.get('/celebrities/:id', async (req, res, next) => {
   try {
-    const movies = await Movie.find();
-    res.render('movies', { movies });
+    const celebrities = await Celebrity.findById(id);
+    res.render('celebrities/show', { celebrities });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/celebrities/new', async (req, res, next) => {
+  const {_id, name, occupation, catchPhrase} = req.body;
+  const celeb = { name, occupation, catchPhrase};
+  try {
+    await Celebrity.create(celeb);
+    //const celebrities = await Celebrity.findById(id);
+    //res.render('celebrities/new', { celebrities });
+    res.redirect('/celebrities');
+  } catch (error) {
+    res.render('/celebrities/new')
+    next(error);
+  }
+});
+
+const movie = await Movie.findById(id);
+console.log(movie);
+res.render('detail', movie);
+
+router.get('/', async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.find();
+    res.render('movies', { celebrities });
   } catch (error) {
     next(error);
   }
